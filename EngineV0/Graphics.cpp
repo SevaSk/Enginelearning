464664140fc5302b6004aec7bd141a29a33e2192
Seva Skvortsov <Seva.sk@gmail.com>
2019-05-08 17:17:09 -0400

    started diagnostics

--- EngineV0/Graphics.cpp
+++ EngineV0/Graphics.cpp
@@ -1,6 +1,12 @@
 #include "Graphics.h"
+#include "dxerr.h"
+#include <sstream>
 
 #pragma comment(lib,"d3d11.lib")
+
+#define GFX_THROW_FAILED(hrcall) if (FAILED(hr = (hcall))) throw Graphics::HrException(__LINE__,__FILE__,hr)
+#define GFX_DEVICE_REMOVED_EXCEPT(hr) Graphics::DeviceRemovedException(__LINE__,__FILE__,(hr))
+
 
 Graphics::Graphics(HWND hWnd)
 {
@@ -20,7 +26,10 @@ Graphics::Graphics(HWND hWnd)
 	sd.Windowed = TRUE;
 	sd.SwapEffect = DXGI_SWAP_EFFECT_DISCARD;
 	sd.Flags = 0;
-	//Create device front/back buffers, and swap chain and rendering context
+	//for checking results of d3d functions
+	HRESULT hr;
+	//Create device front/back	buffers, and swap chain and rendering context
+	//TODO wrapper with error macro
 	D3D11CreateDeviceAndSwapChain(
 		nullptr,
 		D3D_DRIVER_TYPE_HARDWARE,
--- EngineV0/Graphics.h
+++ EngineV0/Graphics.h
@@ -6,6 +6,29 @@
 class Graphics
 {
 public:
+	class Exception : public MyException
+	{
+		using MyException::MyException;
+	};
+	class HrException : public Exception
+	{
+	public:
+		HrException(int line, const char* file, HRESULT hr) noexcept;
+		const char* what() const noexcept override;
+		const char* GetType() const noexcept override;
+		HRESULT GetErrorCode() const noexcept;
+		std::string GetErrorString() const noexcept;
+		std::string GetErrorDescription() const noexcept;
+	private:
+		HRESULT hr;
+	};
+	class DeviceRemovedException : public HrException
+	{
+		using HrException::HrException;
+	public:
+		const char* GetType() const noexcept override;
+	};
+public:
 	Graphics(HWND hWnd);
 	Graphics(const Graphics&) = delete;
 	Graphics& operator=(const Graphics&) = delete;
