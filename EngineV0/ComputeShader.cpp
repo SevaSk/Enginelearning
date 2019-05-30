2aa3af8df58d009a74db037a43a15272c8f7ff36
SevaSk <sevaskgit@gmail.com>
2019-05-30 14:49:15 -0400

    continued to develop my compute shader

--- EngineV0/ComputeShader.cpp
+++ EngineV0/ComputeShader.cpp
@@ -3,29 +3,79 @@
 
 ComputeShader::ComputeShader(Graphics& gfx, const LPCWSTR path)
 {
-	INFOMAN(gfx);
-	LPCSTR profile = (gfx.pDevice.Get()->GetFeatureLevel() >= D3D_FEATURE_LEVEL_11_0) ? "cs_5_0" : "cs_4_0";
+	unsigned int THREAD_GRID_SIZE_X = 20;
+	unsigned int THREAD_GRID_SIZE_Y = 20;
 
+	INFOMAN(gfx);
+
+	//creating structered buffer
+	struct BufferStruct
+	{
+		UINT color;
+	};
+
+	D3D11_BUFFER_DESC sbDesc = {};
+	sbDesc.BindFlags = D3D11_BIND_UNORDERED_ACCESS | D3D11_BIND_SHADER_RESOURCE;
+	sbDesc.CPUAccessFlags = 0;
+	sbDesc.MiscFlags = D3D11_RESOURCE_MISC_BUFFER_STRUCTURED;
+	sbDesc.StructureByteStride = sizeof(BufferStruct);
+	sbDesc.ByteWidth = sizeof(BufferStruct) * THREAD_GRID_SIZE_X * THREAD_GRID_SIZE_Y;
+	sbDesc.Usage = D3D11_USAGE_DEFAULT;
+	
+	Microsoft::WRL::ComPtr<ID3D11Buffer>  pStructuredBuffer;
+	GFX_THROW_INFO(gfx.pDevice.Get()->CreateBuffer(&sbDesc, nullptr, &pStructuredBuffer));
+
+	//creating UAV
+	D3D11_UNORDERED_ACCESS_VIEW_DESC sbUAVDesc;
+	sbUAVDesc.Buffer.FirstElement = 0;
+	sbUAVDesc.Buffer.Flags = 0;
+	sbUAVDesc.Buffer.NumElements = THREAD_GRID_SIZE_X * THREAD_GRID_SIZE_Y;
+	sbUAVDesc.Format = DXGI_FORMAT_UNKNOWN;
+	sbUAVDesc.ViewDimension = D3D11_UAV_DIMENSION_BUFFER;
+
+	Microsoft::WRL::ComPtr<ID3D11UnorderedAccessView> pStructuredBufferUAV;
+	GFX_THROW_INFO(gfx.pDevice.Get()->CreateUnorderedAccessView(pStructuredBuffer.Get(), &sbUAVDesc, &pStructuredBufferUAV));
+
+	//creating Shader Resource View
+	D3D11_SHADER_RESOURCE_VIEW_DESC sbSRVDesc;
+	sbSRVDesc.Buffer.ElementOffset = 0;
+	sbSRVDesc.Buffer.ElementWidth = sizeof(BufferStruct);
+	sbSRVDesc.Buffer.FirstElement = 0;
+	sbSRVDesc.Buffer.NumElements = THREAD_GRID_SIZE_X * THREAD_GRID_SIZE_Y;
+	sbSRVDesc.Format = DXGI_FORMAT_UNKNOWN;
+	sbSRVDesc.ViewDimension = D3D11_SRV_DIMENSION_BUFFER;
+
+	Microsoft::WRL::ComPtr<ID3D11ShaderResourceView> pStructuredBufferSRV;
+	GFX_THROW_INFO(gfx.pDevice.Get()->CreateShaderResourceView(pStructuredBuffer.Get(), &sbSRVDesc, &pStructuredBufferSRV));
+
+
+	//compile and create the compute shader
+	LPCSTR profile = (gfx.pDevice.Get()->GetFeatureLevel() >= D3D_FEATURE_LEVEL_11_0) ? "cs_5_0" : "cs_4_0";
 	Microsoft::WRL::ComPtr<ID3DBlob> pBlob;
 	Microsoft::WRL::ComPtr<ID3DBlob> pErrorBlob;
 
-	GFX_THROW_INFO(D3DCompileFromFile(
-		L"ComputeShader.hlsl",
-		nullptr,
-		D3D_COMPILE_STANDARD_FILE_INCLUDE,
-		"CSMain",
-		profile,
-		D3DCOMPILE_PARTIAL_PRECISION || D3DCOMPILE_ENABLE_STRICTNESS,
-		0,
-		&pBlob,
-		&pErrorBlob));
+	GFX_THROW_INFO(D3DCompile("ComputeShader.hlsl", std::strlen("ComputeShader.hlsl"), NULL, NULL, NULL, "main", profile, 0, 0, &pBlob, &pErrorBlob));
+	GFX_THROW_INFO(gfx.pDevice.Get()->CreateComputeShader(pBlob->GetBufferPointer(), pBlob->GetBufferSize(), NULL, &pComputeShader));
 
-	GFX_THROW_INFO(gfx.pDevice.Get()->CreateComputeShader(pBlob->GetBufferPointer(),
-		pBlob->GetBufferSize(),
-		nullptr,
-		&pComputeShader));
+
+	//dispatch threads
+	UINT initCounts = 0;
+	gfx.pContext->CSSetUnorderedAccessViews(0, 1, pStructuredBufferUAV.GetAddressOf(),&initCounts);
+	gfx.pContext->CSSetShader(pComputeShader.Get(), NULL, 0);
+	gfx.pContext->Dispatch((UINT)20, (UINT)20, 1);
+
+	ID3D11UnorderedAccessView* pNullUAV = NULL;
+	gfx.pContext->CSSetUnorderedAccessViews(0, 1, &pNullUAV, &initCounts);
 
 }
+
+
+
+
+
+
+
+
 
 DxgiInfoManager & ComputeShader::GetInfoManager(Graphics & gfx) noexcept(!IS_DEBUG)
 {
--- EngineV0/ComputeShader.hlsl
+++ EngineV0/ComputeShader.hlsl
@@ -1,5 +1,21 @@
-[numthreads(1, 1, 1)]
-void main( uint3 DTid : SV_DispatchThreadID )
-{
+struct BufferStruct {
+	uint4 color;
+};
 
+static const int THREAD_GROUP_SIZE_X = 20;
+static const int THREAD_GROUP_SIZE_Y = 20;
+static const int THREAD_GROUPS_X = 20;
+
+RWStructuredBuffer<BufferStruct> OutBuff;
+
+[numthreads(THREAD_GROUP_SIZE_X, THREAD_GROUP_SIZE_Y, 1)]
+void main(uint3 threadIDInGroup : SV_GroupThreadID, uint3 groupID : SV_GroupID) 
+{
+	float4 color = float4((float)threadIDInGroup.x / THREAD_GROUP_SIZE_X,
+		(float)threadIDInGroup.y / THREAD_GROUP_SIZE_Y, 0, 1
+		) * 255;
+	int buffIndex = (groupID.y * THREAD_GROUP_SIZE_Y + threadIDInGroup.y)
+		* THREAD_GROUPS_X * THREAD_GROUP_SIZE_X
+		+ (groupID.x * THREAD_GROUP_SIZE_X + threadIDInGroup.x);
+	OutBuff[buffIndex].color = color;
 }
