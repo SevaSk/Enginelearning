4c427339f6842f513caf1003869dddba789bc3d1
SevaSk <sevaskgit@gmail.com>
2020-06-21 15:46:15 -0400

    new start

--- EngineV0/App.cpp
+++ EngineV0/App.cpp
@@ -2,14 +2,75 @@
 #include <sstream>
 #include <iomanip>
 #include <memory>
+#include "Pyramid.h"
+#include "Box.h"
+#include "Melon.h"
 #include "Math.h"
+#include "Sheet.h"
+#include "SkinnedBox.h"
 #include "ComputeShaderGraph.h"
 
 App::App():
 	wnd(800,600, "SK Engine")
 {
-	
-	drawables.push_back(std::make_unique<ComputeShaderGraph>(wnd.Gfx()));
+	class Factory
+	{
+	public:
+		Factory(Graphics& gfx)
+			:
+			gfx(gfx)
+		{}
+		std::unique_ptr<Drawable> operator()()
+		{
+			switch (typedist(rng, 5))
+			{
+			case 0:
+				return std::make_unique<Pyramid>(
+					gfx, rng, adist, ddist,
+					odist, rdist
+					);
+			case 1:
+				return std::make_unique<Box>(
+					gfx, rng, adist, ddist,
+					odist, rdist, bdist
+					);
+			case 2:
+				return std::make_unique<Melon>(
+					gfx, rng, adist, ddist,
+					odist, rdist, longdist, latdist
+					);
+			case 3:
+				return std::make_unique<Sheet>(
+					gfx, rng, adist, ddist,
+					odist, rdist
+					);
+			case 4:
+				return std::make_unique<SkinnedBox>(
+					gfx, rng, adist, ddist,
+					odist, rdist
+					);
+			default:
+				assert(false && "bad drawable type in factory");
+				return {};
+			}
+		}
+	private:
+		Graphics& gfx;
+		std::mt19937 rng{ std::random_device{}() };
+		std::uniform_real_distribution<float> adist{ 0.0f,PI * 2.0f };
+		std::uniform_real_distribution<float> ddist{ 0.0f,PI * 0.5f };
+		std::uniform_real_distribution<float> odist{ 0.0f,PI * 0.08f };
+		std::uniform_real_distribution<float> rdist{ 6.0f,20.0f };
+		std::uniform_real_distribution<float> bdist{ 0.4f,3.0f };
+		std::uniform_int_distribution<int> latdist{ 5,20 };
+		std::uniform_int_distribution<int> longdist{ 10,40 };
+		std::uniform_int_distribution<int> typedist{ 0,2 };
+	};
+
+	Factory f(wnd.Gfx());
+	drawables.reserve(nDrawables);
+	std::generate_n(std::back_inserter(drawables), nDrawables, f);
+
 	wnd.Gfx().SetProjection(DirectX::XMMatrixPerspectiveLH(1.0f, 3.0f / 4.0f, 0.5f, 40.0f));
 }
 
